8e0ec3b49773e5a0df7240add7e7c24b3da501f0
jpmcga <jpaulmcgann@gmail.com>
2024-12-09 14:01:12 -0500

    feat: first working version of merge-task

--- wf/task.py
+++ wf/task.py
@@ -9,11 +9,11 @@ import snapatac2 as snap
 import squidpy as sq
 import subprocess
 
-from typing import List, Tuple
+from typing import List
 
 from latch import message
 from latch.resources.tasks import custom_task
-from latch.types import LatchDir, LatchFile
+from latch.types import LatchDir
 
 import wf.features as ft
 import wf.plotting as pl
@@ -193,41 +193,12 @@ def snap_task(
     logging.info("Writing combined anndata with peaks ...")
     adata.write(f"{out_dir}/combined.h5ad")
 
-    # Move scanpy plots
-    subprocess.run([f"mv /root/figures/* {figures_dir}"], shell=True)
-
-    logging.info("Uploading data to Latch ...")
-    return LatchDir(out_dir, f"latch:///snap_outs/{project_name}")
-
-
-@custom_task(cpu=32, memory=128, storage_gib=4949)
-def motif_task(
-    input_dir: LatchDir,
-    runs: List[utils.Run],
-    genome: utils.Genome,
-    project_name: str
-) -> Tuple[LatchFile, LatchDir]:
-    """Get Anndata object with motifs matrix from cluster peak matrix.  We
-    seperated into a seperate task because of the high memory requirements.
-    """
-
-    logging.info("Downloading data from previous step...")
-    anndata_path = f"{input_dir.local_path}/cluster_peaks.h5ad"
-    cluster_peaks = anndata.read_h5ad(anndata_path)
-
-    groups = utils.get_groups(runs)
-
-    out_dir = "/root/motifs"
-    os.makedirs(out_dir, exist_ok=True)
-
-    figures_dir = f"{out_dir}/figures"
-    os.makedirs(figures_dir, exist_ok=True)
-
-    logging.info("Downloading reference genome for motifs...")
-    genome = genome.value  # Convert to str
-    fasta = utils.get_genome_fasta(genome)
+    # Motifs ------------------------------------------------------------------
+    # Get Anndata object with motifs matrix from cluster peak matrix.
+    cluster_peaks = peak_mats[group]
 
     logging.info("Preparing peak matrix for motifs...")
+    fasta = utils.get_genome_fasta(genome)
     cluster_peaks = ft.get_motifs(cluster_peaks, fasta.local_path)
     cluster_peaks.write("cluster_peaks.h5ad")
 
@@ -258,22 +229,14 @@ def motif_task(
         save="motifs"
     )
 
+    adata_motif.write(f"{out_dir}/combined_motifs.h5ad")
+
+    logging.info("Uploading data to Latch ...")
+    
     # Move scanpy plots
     subprocess.run([f"mv /root/figures/* {figures_dir}"], shell=True)
 
-    adata_motif.write(f"{out_dir}/combined_motifs.h5ad")
-
-    logging.info("Uploading motif data to Latch ...")
-    return (
-        LatchFile(
-            "cluster_peaks.h5ad",
-            f"latch:///snap_outs/{project_name}/cluster_peaks.h5ad"
-        ),
-        LatchDir(
-            out_dir,
-            f"latch:///snap_outs/{project_name}/motifs"
-        )
-    )
+    return LatchDir(out_dir, f"latch:///snap_outs/{project_name}")
 
 
 if __name__ == "__main__":
