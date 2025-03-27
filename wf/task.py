50c40be06c69f57664b48457970108303d9b7d87
jpmcga <jpaulmcgann@gmail.com>
2025-03-27 14:41:29 -0400

    get rid of group neighborhood plots.

--- wf/task.py
+++ wf/task.py
@@ -143,8 +143,10 @@ def make_adata(
     # Neighbrohood enrichment plot, Ripley's plot
     adata = sp.squidpy_analysis(adata)
 
-    neighbor_groups = [g for g in groups if g != "cluster"]
-    group_dict = {g: adata.obs[g].unique() for g in neighbor_groups}
+    # Quick fix for neighborhoods issue.
+    # neighbor_groups = [g for g in groups if g != "cluster"]
+    # group_dict = {g: adata.obs[g].unique() for g in neighbor_groups}
+    group_dict = dict()
     group_dict["all"] = None
 
     for group in group_dict.keys():
