797a644411eb171db53e8959bc9a924c9f8996ed
jpmcga <jpaulmcgann@gmail.com>
2026-01-08 12:57:13 -0500

    bump to streamline 3

--- wf/task.py
+++ wf/task.py
@@ -301,12 +301,12 @@ def motifs_task(
                     template_id="537",
                     widgets=[
                         Widget(
-                            transform_id="120027",
+                            transform_id="131091",
                             key="data_path",
                             value=f"latch:///snap_outs/{project_name}"
                         ),
                         Widget(
-                            transform_id="129103",
+                            transform_id="131100",
                             key="coverages_genome",
                             value=genome
                         )
